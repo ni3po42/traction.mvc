7425227643a2a166dbbd01226c76bf68922670f9
Tim Stratton <ni3po42@gmail.com>
2013-08-02 14:57:27 -0500

    removing unwanted files

--- Demo/build/source/buildConfig/test/ni3po42/android/amvvmdemo/test/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package ni3po42.android.amvvmdemo.test;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-
-}
--- amvvm/build/source/buildConfig/release/amvvm/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package amvvm;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = false;
-
-}
